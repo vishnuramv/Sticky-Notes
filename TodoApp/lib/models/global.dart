edf4298164eaada03398ace8b7cb4d402755954d
Vishnu Ram V <vishnu.v1902@gmail.com>
2020-07-18 14:05:13 +0530

    Created Topbar

---
+++ TodoApp/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGrey = new Color(0xFF212128);
+Color redPink = new Color(0xFFDC4F64);
+
+TextStyle intrayTitle = new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGrey,
+    fontSize: 50);
